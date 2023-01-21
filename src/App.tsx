9537e67c111140c3c8038aa536a47873f144874e
Konixy <konixy.p@gmail.com>
2023-01-21 18:41:49 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { genCube } from './cubeGenerator';
+
+export type Color = 'white' | 'yellow' | 'blue' | 'green' | 'orange' | 'red';
+export type CubeType = [Color[], Color[], Color[], Color[], Color[], Color[]];
+
+const hexFromColor = {
+  white: '#f0f0f0',
+  yellow: 'yellow',
+  blue: '#1d349e',
+  green: 'green',
+  orange: 'orange',
+  red: 'red',
+};
 
 function App() {
-  return <div className="text-xl">app</div>;
+  return (
+    <div className="right-[50%] translate-x-[50%]">
+      <Cube />
+    </div>
+  );
+}
+
+function Cube() {
+  const [cube, setCube] = useState<CubeType>(genCube());
+  return (
+    <div className="">
+      {cube.map((face) => (
+        <div className="grid w-[68px] grid-cols-3 gap-1" key={face.toString()}>
+          {face.map((e) => (
+            <Square color={e} key={Math.random()} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Square({ color }: { color: Color }) {
+  return <div className="h-5 w-5" style={{ backgroundColor: hexFromColor[color] }}></div>;
 }
 
 export default App;
